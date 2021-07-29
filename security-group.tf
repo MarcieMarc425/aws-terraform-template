b9674012b206884873c9d399b55a598bbbba0fe2
Marc Tse <marctse@gmail.com>
2021-07-30 01:21:04 +0800

    feat: Added security groups

---
+++ security-group.tf
@@ -0,0 +1,127 @@
+# Create Security Group for the Application Load Balancer
+resource "aws_security_group" "alb-security-group" {
+  name        = "ALB Security Group"
+  description = "Enable HTTP/HTTPS access on Port 80/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALB Security Group"
+  }
+}
+
+# Create security group for Bastion Host
+resource "aws_security_group" "bastion-security-group" {
+  name        = "Bastion Security Group"
+  description = "Enable SSH access on Port 22"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Bastion Security Group"
+  }
+}
+
+# Create security group for web server
+resource "aws_security_group" "web-security-group" {
+  name        = "Web Security Group"
+  description = "Enable HTTP/HTTPS access on Port 80/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web Server Security Group"
+  }
+}
+
+# Create security group for PostgreSQL server
+resource "aws_security_group" "db-security-group" {
+  name        = "DB Security Group"
+  description = "Enable PostgreSQL access on Port 5432"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "PostgreSQL Access"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "DB Security Group"
+  }
+}
